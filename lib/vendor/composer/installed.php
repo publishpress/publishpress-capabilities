b3950a8df37f880efab7f8996ec396cfbfe39030
olatechpro <olawale.olatech.pro@gmail.com>
2026-03-24 15:50:55 +0100

    composer translate

--- lib/vendor/composer/installed.php
+++ lib/vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-master',
         'version' => 'dev-master',
-        'reference' => '9962df92f0d4cc5d6bc21139ec733728adf5c938',
+        'reference' => '3d3ad2f4a1e447af5b630513f50740191f90f049',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-master',
             'version' => 'dev-master',
-            'reference' => '9962df92f0d4cc5d6bc21139ec733728adf5c938',
+            'reference' => '3d3ad2f4a1e447af5b630513f50740191f90f049',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
