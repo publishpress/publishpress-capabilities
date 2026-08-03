98ccf861d169203d33dfbff2304912d0328f6011
olatechpro <olawale.olatech.pro@gmail.com>
2026-08-03 02:44:35 +0100

    Update Admin Columns installer version to 2.50.1

--- classes/pp-capabilities-installer.php
+++ classes/pp-capabilities-installer.php
@@ -48,7 +48,7 @@ class PP_Capabilities_Installer
             self::migrateAdminNoticesDashboardFeatureStatus();
         }
 
-        if (version_compare($currentVersions, '2.50.0', '<')) {
+        if (version_compare($currentVersions, '2.50.1', '<')) {
             self::addAdminColumnsCapabilities();
         }
 
